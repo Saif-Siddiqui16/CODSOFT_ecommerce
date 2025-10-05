413f783fabf8223ac2c9fb2ced2a7e63b16f1d5c
Mohd. Saif Siddiqui <siddiqui16saif@gmail.com>
2025-10-05 12:27:59 +0530

    middleare

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,18 @@
+import jwt from "jsonwebtoken";
+export const authController = async (req, res, next) => {
+  const token = req.cookies?.token;
 
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.CLIENT_SECRET_KEY);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token." });
+  }
+};
