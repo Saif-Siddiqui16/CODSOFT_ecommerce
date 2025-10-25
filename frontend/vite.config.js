67b5f5742065c5512420c915d39d5b784e4b8bc4
Mohd. Saif Siddiqui <siddiqui16saif@gmail.com>
2025-10-25 11:47:25 +0530

    change in package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    test: {
-        globals: true,
-        environment: "jsdom",
-        setupFiles: "./src/setupTests.ts",
-    },
+  },
 });
